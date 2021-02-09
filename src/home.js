d0ab49ff50f16762b5a8572e211ad16cf0c0f471
sushil07 <sushil29121997@gmail.com>
2021-02-10 04:14:09 +0530

    first

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Home extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Home</h1>
+                <h2>Welcome</h2>
+            </>
+        )
+    }
+}
+
+export default Home
---
+++ src/status.js
@@ -0,0 +1,11 @@
+import React from "react"
+class Status extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Status</h1>
+            </>
+        )
+    }
+}
+export default Status
